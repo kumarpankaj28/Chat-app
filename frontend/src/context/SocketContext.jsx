80ee3d1dfbaf7fa020c34f007a32f375b1260c45
Pankaj Kumar <developer.osizone21@gmail.com>
2024-05-31 19:36:18 +0530

    soket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("https://chat-app-3twn.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
